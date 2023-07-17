36dc0e09c53cf4ff4f22fbee4a26e6b5aff5f29b
mrangad0007 <mrangad007@gmail.com>
2023-07-17 15:20:54 +0530

    prepared for play store

--- build/app/generated/source/buildConfig/release/com/cdinv/chatapp/BuildConfig.java
+++ build/app/generated/source/buildConfig/release/com/cdinv/chatapp/BuildConfig.java
@@ -5,7 +5,7 @@ package com.cdinv.chatapp;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "com.cdinv.chatapp";
+  public static final String APPLICATION_ID = "com.cdinv.chathub";
   public static final String BUILD_TYPE = "release";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.0";
